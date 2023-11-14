bcd454a2ee3029d3bf5491af85c1b1258521fd43
unknown <gogotkd24@gmail.com>
2023-11-14 21:37:27 +0200

    Working on js logic for reaching the API. Implementing GET methods. Slight adjustments to index.html

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,40 @@
-﻿function GetDailyStatuses() {
-
+﻿const uri = '/Status';
+function getStatuses(timeframe) {
+    fetch(uri + `/${timeframe}`)
+        .then(response => response.json())
+        .then(data => DisplayStatuses(data))
+        .catch(error => console.log(error));
 }
+
+function displayStatuses(data) {
+    console.log(data);
+    const BreakTable = document.GetElementById("BreakTable");
+    const LunchTable = document.GetElementById("LunchTable");
+    const MeetingTable = document.GetElementById("MeetingTable");
+
+    for (let status in data) {
+        switch (status.StatusType) {
+            case 'Break':
+                addTableEntry(BreakTable, status);
+            case 'Lunch':
+                addTableEntry(LunchTable, status);
+            case 'Meeting':
+                addTableEntry(MeetingTable, status);
+        }
+    }
+}
+
+function addTableEntry(table, status) {
+    table.insertRow(-1);
+    let count = 0;
+    let c1 = table.insertCell(0);
+    let c2 = table.insertCell(1);
+    let c3 = table.insertCell(2);
+    let c4 = table.insertCell(4);
+
+    c1.innerText = status.id;
+    c2.innerText = status.Timestamp;
+    c3.innerText = status.MinutesLogged;
+    c4.innerText = status.DeviationFromTarget;
+}
+
